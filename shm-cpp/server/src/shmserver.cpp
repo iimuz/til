0b7449a1742e01c52361651aa2fe96bb35d9d62a
iimuz <d.is.iimuz@gmail.com>
2018-03-15 13:30:44 +0000

    feat: add boost::interprocess shared_memory_object

--- shm-cpp/server/src/shmserver.cpp
+++ shm-cpp/server/src/shmserver.cpp
@@ -1,8 +1,14 @@
 #include <cstdlib>
 #include <iostream>
 
+#include <boost/interprocess/shared_memory_object.hpp>
+
 /// @brief IPC用の共有メモリを提供するサーバ側のエントリポイント
 int main() {
-  std::cout << "shared memory server\n";
+  std::cout << "start hared memory server\n";
+
+  boost::interprocess::shared_memory_object shmObj(boost::interprocess::create_only, "shared_memory", boost::interprocess::read_write);
+
+  std::cout << "end shared memory server\n";
   return EXIT_SUCCESS;
 }
