a87af5db10e408751fbc147ca225f43e1ed736ae
iimuz <d.is.iimuz@gmail.com>
2018-03-19 03:32:37 +0000

    feat: initialize shared memory sample using golang

---
+++ shm-go/server/src/shmgoservermain.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+// command line tool info
+type cl struct {
+	OutStream io.Writer
+	ErrStream io.Writer
+}
+
+// version info
+var (
+	version  string
+	revision string
+)
+
+// exit status
+const (
+	ExitCodeOK int = iota
+	ExitCodeError
+)
+
+// main entrypoint
+func main() {
+	instance := &cl{OutStream: os.Stdout, ErrStream: os.Stderr}
+	os.Exit(instance.run(os.Args))
+}
+
+func (c *cl) run(args []string) int {
+	fmt.Fprintf(c.OutStream, "start shared memory using golang.\n")
+
+	fmt.Fprintf(c.OutStream, "end shared memory using golang.\n")
+	return ExitCodeOK
+}
