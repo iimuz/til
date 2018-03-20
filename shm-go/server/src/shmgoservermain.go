3521ff562f76ddb4f999d504ef33002efdf3f159
iimuz <d.is.iimuz@gmail.com>
2018-03-20 03:48:18 +0000

    feat: add shared memory package

--- shm-go/server/src/shmgoservermain.go
+++ shm-go/server/src/shmgoservermain.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/fabiokung/shm"
 	"io"
 	"os"
+	"syscall"
 )
 
 // command line tool info
@@ -32,6 +34,25 @@ func main() {
 
 func (c *cl) run(args []string) int {
 	fmt.Fprintf(c.OutStream, "start shared memory server using golang.\n")
+
+	file, err := shm.Open("my_region", os.O_RDWR|os.O_CREATE, 0600)
+	if err != nil {
+		fmt.Fprintf(c.ErrStream, "shared memory open error: %v", err)
+		return ExitCodeError
+	}
+	defer file.Close()
+	defer shm.Unlink(file.Name())
+
+	if err := syscall.Ftruncate(int(file.Fd()), 1024); err != nil {
+		fmt.Fprintf(c.ErrStream, "shared memory truncate error: %v", err)
+		return ExitCodeError
+	}
+
+	data := uint8(100)
+	if err := file.Write(([]byte)(1)); err != nil {
+		fmt.Fprintf(c.ErrStream, "shared memory write error: %v", err)
+		return ExitCodeError
+	}
 
 	fmt.Fprintf(c.OutStream, "end shared memory server using golang.\n")
 	return ExitCodeOK
