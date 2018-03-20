011e267a2d17c3d4eec973e5d402f85306dd44e0
iimuz <d.is.iimuz@gmail.com>
2018-03-20 08:19:28 +0000

    feat: add client

--- shm-go/server/src/shmgoservermain.go
+++ shm-go/server/src/shmgoservermain.go
@@ -37,7 +37,8 @@ func main() {
 func (c *cl) run(args []string) int {
 	fmt.Fprintf(c.OutStream, "start shared memory server using golang.\n")
 
-	name := string("shmgo")
+	// 共有メモリのエントリポイントを作成
+	name := string("shared_memory")
 	file, err := shm.Open(name, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0600)
 	if err != nil {
 		fmt.Fprintf(c.ErrStream, "open error: %v\n", err)
@@ -46,11 +47,13 @@ func (c *cl) run(args []string) int {
 	defer file.Close()
 	defer shm.Unlink(file.Name())
 
+	// 共有メモリの容量を確保
 	if err := syscall.Ftruncate(int(file.Fd()), int64(1024)); err != nil {
 		fmt.Fprintf(c.ErrStream, "truncate error: %v\n", err)
 		return ExitCodeError
 	}
 
+	// 共有メモリのポインタを取得([]byte)
 	fd := int(uintptr(file.Fd()))
 	buff, err := syscall.Mmap(fd, 0, 1024, syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_SHARED)
 	if err != nil {
@@ -58,6 +61,7 @@ func (c *cl) run(args []string) int {
 		return ExitCodeError
 	}
 
+	// unsafeを利用して[]intに変換し、代入
 	const LOOP_NUM = 10
 	b := (*[256]int)(unsafe.Pointer(&buff[0]))[:]
 	for i := 0; i < LOOP_NUM; i++ {
