3daf38b6ba077864a2837e9f92e5fc0f39b15a58
iimuz <d.is.iimuz@gmail.com>
2018-03-20 08:04:57 +0000

    feat: add server

--- shm-go/server/src/shmgoservermain.go
+++ shm-go/server/src/shmgoservermain.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"os"
 	"syscall"
+	"time"
+	"unsafe"
 )
 
 // command line tool info
@@ -26,7 +28,7 @@ const (
 	ExitCodeError
 )
 
-// main entrypoint
+// main is entrypoint of this binary
 func main() {
 	instance := &cl{OutStream: os.Stdout, ErrStream: os.Stderr}
 	os.Exit(instance.run(os.Args))
@@ -35,23 +37,33 @@ func main() {
 func (c *cl) run(args []string) int {
 	fmt.Fprintf(c.OutStream, "start shared memory server using golang.\n")
 
-	file, err := shm.Open("my_region", os.O_RDWR|os.O_CREATE, 0600)
+	name := string("shmgo")
+	file, err := shm.Open(name, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0600)
 	if err != nil {
-		fmt.Fprintf(c.ErrStream, "shared memory open error: %v", err)
+		fmt.Fprintf(c.ErrStream, "open error: %v\n", err)
 		return ExitCodeError
 	}
 	defer file.Close()
 	defer shm.Unlink(file.Name())
 
-	if err := syscall.Ftruncate(int(file.Fd()), 1024); err != nil {
-		fmt.Fprintf(c.ErrStream, "shared memory truncate error: %v", err)
+	if err := syscall.Ftruncate(int(file.Fd()), int64(1024)); err != nil {
+		fmt.Fprintf(c.ErrStream, "truncate error: %v\n", err)
 		return ExitCodeError
 	}
 
-	data := uint8(100)
-	if err := file.Write(([]byte)(1)); err != nil {
-		fmt.Fprintf(c.ErrStream, "shared memory write error: %v", err)
+	fd := int(uintptr(file.Fd()))
+	buff, err := syscall.Mmap(fd, 0, 1024, syscall.PROT_READ|syscall.PROT_WRITE, syscall.MAP_SHARED)
+	if err != nil {
+		fmt.Fprintf(c.ErrStream, "mmap error: %v\n", err)
 		return ExitCodeError
+	}
+
+	const LOOP_NUM = 10
+	b := (*[256]int)(unsafe.Pointer(&buff[0]))[:]
+	for i := 0; i < LOOP_NUM; i++ {
+		b[0] = i
+		fmt.Fprintf(c.OutStream, "%v\n", b[0])
+		time.Sleep(1 * time.Second)
 	}
 
 	fmt.Fprintf(c.OutStream, "end shared memory server using golang.\n")
