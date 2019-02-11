c5b9df41d6301edba4368cddaa0900af640d7f9f
iimuz <iz.primary.story+github@gmail.com>
2019-02-11 10:48:04 +0900

    fix: header を付与

--- hello/index.js
+++ hello/index.js
@@ -6,5 +6,7 @@
  */
 exports.helloWorld = (req, res) => {
   let message = req.query.message || req.body.message || 'Hello World! Update Permission.\n';
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
   res.status(200).send(message);
 };
