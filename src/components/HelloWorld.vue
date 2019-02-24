809bfc061725d4a85143b35fdad8431a02ed5803
iimuz <iz.primary.story+github@gmail.com>
2019-02-24 12:27:18 +0900

    fix: UI を vue のデータバインディングに修正

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="hello">
     <h1>A3RT Text Summarization API Sample</h1>
-    <input class="input" type="text" id="apikey" placeholder="API Key">
-    <textarea class="textarea" id="text" placeholder="Input sentences."></textarea>
-    <a class="button is-primary" id="summarize">Summarize</a>
-    <div class="box" id="result" style="margin-top: 20px">Output</div>
-    <div class="content">
-      Response status: <div id="res-code"></div>
-      Response message: <div id="res-msg"></div>
+    <textarea
+      class="textarea"
+      v-model="inputValue"
+      placeholder="Input sentences.">
+    </textarea>
+    <a class="button is-primary" v-on:click="summarize">Summarize</a>
+    <div class="box" v-model="resultValue" style="margin-top: 20px">
+      {{ resultValue }}
     </div>
   </div>
 </template>
@@ -16,10 +17,17 @@
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
-export default class HelloWorld extends Vue {}
+export default class HelloWorld extends Vue {
+  private apikey: string = '';
+  private inputValue: string = '';
+  private resultValue: string = 'Output';
+
+  private summarize(): void {
+    this.resultValue = this.inputValue;
+  }
+}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="sass" scoped>
 @import "../../node_modules/bulma/bulma.sass";
 </style>
