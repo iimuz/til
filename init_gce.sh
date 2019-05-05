a7e96a2b1703ae9b14cdbfd867a6789b31ccc9df
iimuz <iz.primary.story+github@gmail.com>
2019-05-05 12:52:54 +0900

    docs: 初期化スクリプトで Ubuntu を仮定していることを明記

--- init_gce.sh
+++ init_gce.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 #
 # GCE インスタンス起動時の初期化処理を実行します。
+# 対象のインスタンスは Ubuntu であると仮定しています。
 
-apt update
-apt upgrade -y
-apt autoremove -y
-apt clean
+sudo apt update
+sudo apt upgrade -y
+sudo apt autoremove -y
+sudo apt clean
 
-apt install -y --no-install-recommeds git
+sudo apt install -y --no-install-recommeds git
 
 mkdir -p src/github.com/iimuz && pushd $_
 git clone https://github.com/iimuz/dotfiles.git
 pushd dotfiles
-bash setup_gce.sh
+bash setup_ubuntu.sh
 popd
 popd
 
