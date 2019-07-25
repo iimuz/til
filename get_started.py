36194bf33f1a01926511e6adfb1d9f6dd23eee40
iimuz <iz.primary.story+github@gmail.com>
2019-07-25 20:26:27 +0900

    wip: 出力を追加中

--- get_started.py
+++ get_started.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pickle
+import sklearn.metrics as skmetrics
 import xgboost as xgb
 
 from logging import getLogger
@@ -6,14 +9,38 @@ logger = getLogger(__name__)
 
 
 def main() -> None:
+    # load data
     data_train = xgb.DMatrix("_data/agaricus.txt.train")
     data_test = xgb.DMatrix("_data/agaricus.txt.test")
 
+    # train
     param = {"max_depth": 2, "eta": 1, "objective": "binary:logistic"}
     num_round = 2
+    evals = [(data_train, "train"), (data_test, "eval")]
 
-    model = xgb.train(param, data_train, num_round)
-    predicts = model.predict(data_test)
+    evals_result = {}
+    model = xgb.train(
+        param, data_train, num_round, evals=evals, evals_result=evals_result
+    )
+
+    # predict and calculate score
+    predicted_probability = model.predict(data_test)
+
+    predicted_binary = np.where(predicted_probability > 0.5, 1, 0)
+    accuracy = skmetrics.accuracy_score(data_test.get_label(), predicted_binary)
+    logger.info(f"accuracy score: {accuracy}")
+
+    # output results
+    logger.debug(evals_result)
+    with open("_data/get-started_results.pkl", "wb") as f:
+        pickle.dump(
+            {
+                "accuracy": accuracy,
+                "train error": evals_result["train"]["error"],
+                "eval error": evals_result["eval"]["error"],
+            },
+            f,
+        )
 
 
 if __name__ == "__main__":
