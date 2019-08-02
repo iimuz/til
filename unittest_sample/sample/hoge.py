1068ab051150fae286f1c7b21e90dcc1e3847f6d
iimuz <iz.primary.story+github@gmail.com>
2019-08-02 10:34:39 +0000

    feat: unittest を利用したサンプルコードを追加

---
+++ unittest_sample/sample/hoge.py
@@ -0,0 +1,11 @@
+def check_equal(src1: int, src2: int) -> bool:
+    """入力された 2 つの引数が一致する場合に true を返す
+
+    Args:
+        src1 (int): 一致チェックする第一引数
+        src2 (int): 一致チェックする第二引数
+
+    Returns:
+        bool: true の場合に一致する
+    """
+    return src1 == src2
