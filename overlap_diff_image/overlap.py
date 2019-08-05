2040cb339c20e2fa18044612fbd751e80045bac6
iimuz <iz.primary.story+github@gmail.com>
2019-08-05 06:46:37 +0000

    refact: 外部 yaml を読み込む形式に修正

--- overlap_diff_image/overlap.py
+++ overlap_diff_image/overlap.py
@@ -1,4 +1,6 @@
 import numpy as np
+import sys
+import yaml
 
 from PIL import Image
 from logging import getLogger
@@ -9,29 +11,26 @@ logger = getLogger(__name__)
 def main() -> None:
     """指定したベース画像との差分をオーバーラップした画像を保存する。
     """
-    BASE_IMAGE_NAME = "_data/hoge000.png"
-    DIFF_IMAGE_LIST = [
-        "_data/hoge001.png",
-        "_data/hoge002.png",
-        "_data/hoge003.png",
-        "_data/hoge004.png",
-        "_data/hoge005.png",
-    ]
-    OUTPUT_IMAGE_NAME = "_data/hoge_overlap.png"
+    CONFIG_PATH = "config.yaml" if len(sys.argv) < 2 else sys.argv[1]
+    logger.debug(f"config: {CONFIG_PATH}")
+    with open(CONFIG_PATH) as f:
+        CONFIG = yaml.safe_load(f.read())
 
-    logger.debug(f"load base image: {BASE_IMAGE_NAME}")
-    img_base = np.asarray(Image.open(BASE_IMAGE_NAME)).astype(np.int)
+    logger.debug(CONFIG)
+
+    logger.debug(f"load base image: {CONFIG['base_image_path']}")
+    img_base = np.asarray(Image.open(CONFIG["base_image_path"])).astype(np.int)
 
     img_overlap = img_base.copy()
-    for diff_name in DIFF_IMAGE_LIST:
+    for diff_name in CONFIG["diff_image_list"]:
         logger.debug(f"load image: {diff_name}")
         img_diff = np.asarray(Image.open(diff_name)).astype(np.int)
         img_overlap += np.abs(img_base - img_diff)
     img_overlap[img_overlap < 0] = 0
     img_overlap[img_overlap > 255] = 255
 
-    logger.debug(f"output image: {OUTPUT_IMAGE_NAME}")
-    Image.fromarray(np.uint8(img_overlap)).save(OUTPUT_IMAGE_NAME)
+    logger.debug(f"output image: {CONFIG['output_image_path']}")
+    Image.fromarray(np.uint8(img_overlap)).save(CONFIG["output_image_path"])
 
 
 if __name__ == "__main__":
