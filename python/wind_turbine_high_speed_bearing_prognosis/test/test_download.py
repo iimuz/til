6341f46dfb2fef729c639f7de915db0964a5599b
iimuz <iz.primary.story+github@gmail.com>
2019-11-18 03:44:45 +0000

    wip: load dataset

--- python/wind_turbine_high_speed_bearing_prognosis/test/test_download.py
+++ python/wind_turbine_high_speed_bearing_prognosis/test/test_download.py
@@ -1,6 +1,9 @@
 # default
 import unittest
 
+# third party
+from scipy import io
+
 # my pckages
 import download
 
@@ -11,3 +14,6 @@ class TestDwonload(unittest.TestCase):
 
     def test_download(self):
         download.get_file()
+        var = io.loadmat("data/hs_bearing_1/hs_bearing_1/sensor-20130307T015746Z.mat")
+        for key, item in var.items():
+            print(key, item)
