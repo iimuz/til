14a2eb4b2911f65c5753dd3264dab2c85b000845
iimuz <iz.primary.story+github@gmail.com>
2019-11-25 17:37:27 +0900

    feat: add jupytext sample

---
+++ python/jupytext/sample.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.3.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# # jupytext によるエディタでの編集
+
+# +
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+logger.info("hello world")
