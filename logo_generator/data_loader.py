953e2c8f5328dd4fa830110ed7857aa913cdc731
iimuz <iz.primary.story+github@gmail.com>
2019-11-29 22:46:44 +0900

    feat: add logo_generator

---
+++ logo_generator/data_loader.py
@@ -0,0 +1,21 @@
+import pathlib
+
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+
+def load_mnist(batch_size: int) -> DataLoader:
+    transform = transforms.Compose([transforms.ToTensor()])
+    dataset = datasets.MNIST(
+        "data/mnist", train=True, download=True, transform=transform
+    )
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    return loader
+
+
+def load_icons(path: pathlib.Path, batch_size: int, transform) -> DataLoader:
+    data = datasets.ImageFolder(str(path), transform=transform)
+    loader = DataLoader(data, shuffle=True, batch_size=batch_size, num_workers=4)
+
+    return loader
