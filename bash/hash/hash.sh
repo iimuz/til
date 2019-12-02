b338da06362805a687247d5e2a5055ad079ac30c
iimuz <iz.primary.story+github@gmail.com>
2019-12-02 19:46:32 +0900

    feat: add hash script using bash.

---
+++ bash/hash/hash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# 指定したディレクトリ以下のハッシュ値を再帰的に計算しファイルに保存します。
+
+ROOT_DIR=/path/to/root/dir
+HASH_FILE=/path/to/hash/file
+
+echo "root dir: $ROOT_DIR"
+echo "hash csv: $HASH_FILE"
+
+pushd $ROOT_DIR
+find . -type f | xargs -IXXX sha256sum XXX | tee $HASH_FILE
+popd
+
