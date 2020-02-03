811402fc43fa4a9666c1f513cc958f7f0ad03afc
iimuz <iz.primary.story+github@gmail.com>
2020-02-03 06:04:24 +0000

    refact: using formmater

--- python/docker_with_gpu/check_gpu_using_tf.py
+++ python/docker_with_gpu/check_gpu_using_tf.py
@@ -3,4 +3,4 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import tensorflow as tf
 
 
-print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices("GPU")))
