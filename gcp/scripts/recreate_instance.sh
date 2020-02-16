7717fc6c32a62e22f09ef27c7b9ce4c14d2da14b
iimuz <iz.primary.story+github@gmail.com>
2020-02-16 22:53:01 +0900

    docs: add instance pattern for gce

--- gcp/scripts/recreate_instance.sh
+++ gcp/scripts/recreate_instance.sh
@@ -5,16 +5,19 @@
 # リージョンなどを設定する前提としています。
 
 # * preemptible f1-micro
-#     * 0.6 GB Memory
+#     * 1 vCPU / 0.6 GB Memory
 #     * 0.006 $ / min, 4.17 $ / month
 # * preemptible g1-small
-#     * 1.7 GB Memory
+#     * 1 vCPU / 1.7 GB Memory
 #     * 0.011 $ / min, 7.82 $ / month
 # * preemptible n1-standard-1
-#     * 3.75 GB Memory
+#     * 1 vCPU / 3.75 GB Memory
 #     * 0.014 $ / min, 10.19 $ / month
+# * preemptible e2-standard-2
+#     * 2 vCPU / 8 GB Memory
+#     * 0.021 $ / min, 15.08 $ / month
 INSTANCE_NAME=${1:-dev}
-MACHINE_TYPE=${2:-n1-standard-1}
+MACHINE_TYPE=${2:-f1-micro}
 DISK_NAME=${3:-${INSTANCE_NAME}}
 
 echo "create: name: $INSTANCE_NAME, type: $MACHINE_TYPE, disk: $DISK_NAME"
@@ -25,7 +28,7 @@ gcloud compute instances create $INSTANCE_NAME \
   --network-tier=PREMIUM \
   --no-restart-on-failure \
   --maintenance-policy=TERMINATE \
-  --preemptible \
   --disk=name=${DISK_NAME},device-name=${DISK_NAME},mode=rw,boot=yes \
-  --reservation-affinity=any
+  --reservation-affinity=any \
+  --preemptible
 
