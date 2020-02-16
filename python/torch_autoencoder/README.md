a0ad91ecc377ea5fa635e8b71dee63ed37fd49b6
iimuz <iz.primary.story+github@gmail.com>
2020-02-16 07:33:33 +0000

    fix: change training method

--- python/torch_autoencoder/README.md
+++ python/torch_autoencoder/README.md
@@ -2,3 +2,14 @@
 
 PyTorch を利用した Autoencoder 実装です。
 対象データは、時系列データとしてます。
+
+## 環境構築
+
+Poetry で管理しているため、下記コマンドで環境構築できます。
+
+```sh
+poetry install
+```
+
+ただし、 pytorch 1.4.0 と torchvision, pytorch-lightning が依存関係ではじかれるため、
+別に pytorch-lightning をインストールしてください。
--- python/torch_autoencoder/train.py
+++ python/torch_autoencoder/train.py
@@ -7,12 +7,35 @@ from pytorch_lightning import Trainer
 
 from aetrainer import AETrainer
 
-# from simpleautoencoder import SimpleAutoencoder
-# from simpledeepautoencoder import SimpleDeepAutoencoder
-# from simplecnnautoencoder import SimpleCNNAutoencoder
+from simpleautoencoder import SimpleAutoencoder
+from simpledeepautoencoder import SimpleDeepAutoencoder
+from simplecnnautoencoder import SimpleCNNAutoencoder
 from simplelstm import SimpleLSTM
 
 logger = getLogger(__name__)
+
+
+def _create_network(name: str, sequence_length: int):
+    if name == "SimpleAE":
+        hparams = dict(input_dim=sequence_length, hidden_dim=sequence_length // 2)
+        return SimpleAutoencoder(**hparams), hparams
+
+    if name == "SimpleCNN":
+        hparams = dict(input_channels=1)
+        return SimpleCNNAutoencoder(**hparams), hparams
+
+    if name == "SimpleDNN":
+        hparams = dict(input_dim=sequence_length, hidden_dim=sequence_length // 2)
+        return SimpleDeepAutoencoder(**hparams), hparams
+
+    if name == "SimpleLSTM":
+        hparams = dict(
+            input_size=sequence_length,
+            hidden_size=32,
+            output_size=sequence_length,
+            device="cuda:0" if torch.cuda.is_available() else "cpu",
+        )
+        return SimpleLSTM(**hparams), hparams
 
 
 def _init_rand_seed(seed):
@@ -27,20 +50,17 @@ def _main() -> None:
 
     logging.basicConfig(level=logging.INFO)
 
-    save_path = "_models/lstm"
     gpus = [0] if torch.cuda.is_available() else None
     _init_rand_seed(0)
 
-    # model = SimpleAutoencoder(10, 5).to(device)
-    # model = SimpleDeepAutoencoder(10, 6).to(device)
-    # hparams = dict(input_channels=1)
-    # network = SimpleCNNAutoencoder(**hparams)
-    hparams = dict(input_size=64, hidden_size=32, output_size=64,)
-    network = SimpleLSTM(**hparams)
+    sequence_length = 64
+    name = "SimpleLSTM"
+    network, hparams = _create_network(name, sequence_length)
+    save_path = f"_models/{name}"
 
     model = AETrainer(
         model=network,
-        sequence_length=64,
+        sequence_length=sequence_length,
         batch_size=64,
         scaler_path="_data/interim/scaler.pkl",
         train_path="_data/interim/train.pkl",
