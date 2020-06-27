363c6aa665d51982c50a1496f4a51cf6ea670f92
iimuz <iz.primary.story+github@gmail.com>
2020-06-27 07:14:15 +0000

    wip: ローカル設定
    - colab の同期用スクリプト

--- gcp/colab/sync_local.sh
+++ gcp/colab/sync_local.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-watch_dir=/workspace/hoge
+watch_dir=/content/torch_time_series_forecasting
 ssh_key=$HOME/.ssh/google_compute_engine
-ip=username@hoge.us-west1-b.project
-src_dir=/workspace/hoge/
-dst_dir=/path/to/output/dir
+ip=disii@free.us-west1-b.develop-231513
+src_dir=/content/torch_time_series_forecasting
+dst_dir=/home/disii/src/github.com/iimuz/til/machine_learning
 
 apt-get install -y --no-install-recommends inotify-tools
 while inotifywait -e create,delete,modify,move -r -q ${watch_dir}; do
--- gcp/colab/sync_remote.sh
+++ gcp/colab/sync_remote.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 ssh_option="-p 20022"
-src_dir=/path/to/source
+src_dir=$HOME/src/github.com/iimuz/til/machine_learning/torch_time_series_forecasting
 ip=root@localhost
-dst_dir=/workspace
+dst_dir=/content
 
 rsync -achu --no-o --no-g --delete --progress -e "ssh $ssh_option" $src_dir ${ip}:$dst_dir
 rsync -achu --no-o --no-g --delete --progress -e "ssh $ssh_option" sync_local.sh ${ip}:$dst_dir
+rsync -achu --no-o --no-g --delete --progress -e "ssh $ssh_option" $HOME/src/github.com/iimuz/config/.config/vscode/colab.code-workspace ${ip}:$dst_dir
