c997395251c2a1371e17105c0f37fb88fd7b5c29
iimuz <iz.primary.story+github@gmail.com>
2020-12-12 10:31:52 +0000

    feat: food101のメタデータ読み込みを追加

--- machine_learning/torch_classification_sample/src/data/dataset_food101.py
+++ machine_learning/torch_classification_sample/src/data/dataset_food101.py
@@ -1,10 +1,12 @@
 """Food101データセット."""
 # default packages
+import json
 import logging
 import tarfile
 import urllib.request as request
 
 # third party packages
+import pandas as pd
 import tqdm.autonotebook as tqdm
 
 # my packages
@@ -17,6 +19,18 @@ _logger = logging.getLogger(__name__)
 
 class Food101(dataset.BaseDataset):
     """ローカルのFood101データセットを管理するクラス."""
+
+    def __init__(self, mode: dataset.Mode) -> None:
+        super().__init__(mode=mode)
+
+        self.primary_keys = ["name"]
+
+        self._path_expand = self.path.joinpath("food-101")
+        self._path_images = self._path_expand.joinpath("images")
+        self._path_meta = self._path_expand.joinpath("meta")
+        self._path_classes = self._path_meta.joinpath("classes.txt")
+        self._path_test = self._path_meta.joinpath("test.json")
+        self._path_train = self._path_meta.joinpath("train.json")
 
     def create_dataset(self, pbar: tqdm.tqdm, **kwargs) -> None:
         tarpath = self.path.joinpath("food-101.tar.gz")
@@ -44,12 +58,33 @@ class Food101(dataset.BaseDataset):
                 tar.extractall(self.path)
 
     def load_dataset(self, pbar: tqdm.tqdm, **kwargs) -> None:
-        pass
+        path_list = (
+            self._path_train if self.mode == dataset.Mode.TRAIN else self._path_test
+        )
+        with open(path_list) as f:
+            filelist = json.load(f)
+        self.data_ = pd.DataFrame(
+            [
+                {
+                    "name": path,
+                    "class": key,
+                }
+                for key, files in filelist.items()
+                for path in files
+            ]
+        )
+        self.data_["path"] = self.data_["name"].apply(
+            lambda x: self._path_images.joinpath(x).resolve()
+        )
+
+        with open(self._path_classes) as f:
+            self.classes_ = set([v.rstrip() for v in f.readlines()])
 
 
 def main() -> None:
     """データセットのダウンロードを実行するスクリプト."""
-    Food101().create()
+    target = Food101(dataset.Mode.TRAIN).create().load()
+    _logger.info(target.data_)
 
 
 if __name__ == "__main__":
