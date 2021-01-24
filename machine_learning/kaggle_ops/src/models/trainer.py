65246f01b898a8fb2071f7e4d4378e561ff6e294
iimuz <iz.primary.story+github@gmail.com>
2021-01-24 09:12:58 +0000

    feat: MLFlowによる損失関数の記録を追加

--- machine_learning/kaggle_ops/src/models/trainer.py
+++ machine_learning/kaggle_ops/src/models/trainer.py
@@ -1,7 +1,9 @@
 """PyTorch Lightning用学習モジュール."""
 # default packages
+import dataclasses
 import logging
 import os
+import pathlib
 import sys
 
 # third party packages
@@ -9,6 +11,7 @@ import pytorch_lightning as pl
 import pytorch_lightning.loggers as pl_loggers
 import torch.nn.functional as F
 import torch.optim as optim
+import yaml
 
 # my packages
 import src.data.dataset as dataset
@@ -16,6 +19,13 @@ import src.models.net_cnn as net_cnn
 
 # logger
 _logger = logging.getLogger(__name__)
+
+
+@dataclasses.dataclass
+class Config:
+    """スクリプト実行用設定値."""
+
+    experiment_name: str = "default"
 
 
 class PlModel(pl.LightningModule):
@@ -32,6 +42,8 @@ class PlModel(pl.LightningModule):
         logits = self(x)
         loss = F.nll_loss(logits, y)
 
+        self.log("train_loss", loss)
+
         return loss
 
     def validation_step(self, batch, batch_idx):
@@ -39,16 +51,22 @@ class PlModel(pl.LightningModule):
         logits = self(x)
         loss = F.nll_loss(logits, y)
 
+        self.log("val_loss", loss)
+
         return loss
 
     def configure_optimizers(self):
         return optim.Adam(self.parameters(), lr=1e-3)
 
 
-def main() -> None:
+def main(config: Config) -> None:
+    MLFLOW_TRACKING_URI = os.environ.get(
+        "MLFLOW_TRACKING_URI", "file:./data/processed/mlruns"
+    )
+
     mlf_logger = pl_loggers.MLFlowLogger(
-        experiment_name="default",
-        tracking_uri="file:./data/processed/mlruns",
+        experiment_name=config.experiment_name,
+        tracking_uri=MLFLOW_TRACKING_URI,
     )
     model = PlModel()
     mnist = dataset.Mnist()
@@ -56,11 +74,26 @@ def main() -> None:
     trainer.fit(model, mnist)
 
 
+def _load_config(filepath: pathlib.Path) -> Config:
+    """設定ファイルを読み込む."""
+    with open(str(filepath), "r") as f:
+        data = yaml.load(f, Loader=yaml.SafeLoader)
+    config = Config(**data)
+
+    return config
+
+
 if __name__ == "__main__":
     try:
         debug_mode = True if os.environ.get("MODE_DEBUG", "") == "True" else False
         logging.basicConfig(level=logging.DEBUG if debug_mode else logging.INFO)
-        main()
+
+        if len(sys.argv) == 2:
+            _config = _load_config(pathlib.Path(sys.argv[1]))
+        else:
+            _config = Config()
+
+        main(_config)
     except Exception as e:
         _logger.exception(e)
         sys.exit("Fail")
