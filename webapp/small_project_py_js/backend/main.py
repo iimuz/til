3bc666e3477ea1cb4debecad259c242b59f2daee
iimuz <iz.primary.story+github@gmail.com>
2021-09-30 06:45:12 +0000

    fix: 単純なメッセージを返すように修正

--- webapp/small_project_py_js/backend/main.py
+++ webapp/small_project_py_js/backend/main.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 app = Flask(
     __name__,
-    static_folder="../frontend/dist/static",
+    static_folder="../frontend/dist",
     template_folder="../frontend/dist",
 )
 
@@ -11,6 +11,11 @@ app = Flask(
 @app.route("/", defaults={"path": ""})
 def index(path: str):
     return flask.render_template("index.html")
+
+
+@app.route("/hello")
+def hello():
+    return "hello"
 
 
 if __name__ == "__main__":
