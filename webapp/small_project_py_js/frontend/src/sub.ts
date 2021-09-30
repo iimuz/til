3b8b39ffcdfb37258314b9e1673d7bfec3675941
iimuz <iz.primary.story+github@gmail.com>
2021-09-30 06:44:43 +0000

    fix: 簡易版のhtmlとメッセージ出力ボックスを追加

--- webapp/small_project_py_js/frontend/src/sub.ts
+++ webapp/small_project_py_js/frontend/src/sub.ts
@@ -1,9 +1,26 @@
 export const hello = (message: string): void => {
-  log(message);
+  // log(message);
+  get_message()
 };
 
 function log(message: string): void {
-  // document.body.innerHTML = `${message}`;
+  document.body.innerHTML = `${message}`;
 
   console.log(`output ${message}`);
+}
+
+function get_message(): void {
+  document.getElementById("button").addEventListener("click", loadMessage);
+}
+
+function loadMessage(): void {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "/hello", true)
+  xhr.onload = function() {
+    if (this.status == 200) {
+      var message = this.responseText;
+      document.getElementById("comment").innerHTML = message;
+    }
+  }
+  xhr.send()
 }
