17af77ec065c758dd84b0e22b97edcb3d7509b21
iimuz <iz.primary.story+github@gmail.com>
2021-10-01 08:50:24 +0000

    feat: npm run buildでhtmlがdist下に生成されるように変更

--- webapp/small_project_py_js/backend/main.py
+++ webapp/small_project_py_js/backend/main.py
@@ -4,7 +4,7 @@ from flask import Flask
 app = Flask(
     __name__,
     static_folder="../frontend/dist",
-    template_folder="../frontend/dist",
+    template_folder="../frontend/dist/html",
 )
 
 
--- webapp/small_project_py_js/frontend/webpack.config.js
+++ webapp/small_project_py_js/frontend/webpack.config.js
@@ -1,3 +1,6 @@
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
 module.exports = {
   mode: 'development',
   entry: './src/main.ts',
@@ -12,4 +15,10 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.js'],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "html", "index.html"),
+      filename: "html/index.html"
+    })
+  ],
 };
