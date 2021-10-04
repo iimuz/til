18896f43755321b68d27f97623bce979395a1357
iimuz <iz.primary.story+github@gmail.com>
2021-10-04 01:13:32 +0000

    chore: ビルド時のコマンドにモードを追加したのでwebpack.config.jsから削除

--- webapp/small_project_py_js/frontend/webpack.config.js
+++ webapp/small_project_py_js/frontend/webpack.config.js
@@ -2,7 +2,6 @@ const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-  mode: 'development',
   entry: './src/main.ts',
   output: {
     path: path.resolve(__dirname, "dist"),
