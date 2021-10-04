4fa90988b666afd068b97d6a06847736bd1f4060
iimuz <iz.primary.story+github@gmail.com>
2021-10-04 01:30:22 +0000

    fix: デフォルトで返すパスを設定

--- webapp/small_project_py_js/backend/main.py
+++ webapp/small_project_py_js/backend/main.py
@@ -3,14 +3,21 @@ from flask import Flask
 
 app = Flask(
     __name__,
+    static_url_path="/dist",
     static_folder="../frontend/dist",
     template_folder="../frontend/dist/html",
 )
 
 
 @app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
 def index(path: str):
-    return flask.render_template("index.html")
+    return app.send_static_file("html/index.html")
+
+
+@app.route("/sample.html", defaults={"path": ""})
+def sample(path: str):
+    return app.send_static_file("html/sample.html")
 
 
 @app.route("/hello")
