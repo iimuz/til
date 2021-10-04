b27e353e6801382835902a7c5b6748a5628eb745
iimuz <iz.primary.story+github@gmail.com>
2021-10-04 01:37:05 +0000

    fix: 各ページごとに必要なjsを使うように修正

--- webapp/small_project_py_js/frontend/webpack.config.js
+++ webapp/small_project_py_js/frontend/webpack.config.js
@@ -2,10 +2,14 @@ const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-  entry: './src/main.ts',
+  entry: {
+    "index": "./src/main.ts",
+    "sample": "./src/sample.ts",
+  },
   output: {
     path: path.resolve(__dirname, "dist"),
     publicPath: "dist",
+    filename: "[name].js",
   },
   module: {
     rules: [
@@ -22,12 +26,14 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src", "html", "index.html"),
       filename: "html/index.html",
+      chunks: ["index"],
       inject: "head",
       hash: true,
     }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "src", "html", "sample.html"),
       filename: "html/sample.html",
+      chunks: ["sample"],
       inject: "head",
       hash: true,
     })
